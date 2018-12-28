7f243141da6f9f9f034f90e5f803a7148676022f
timhutch79git config --global user.name tim.hutch.work@gmail.com <tim.hutch.work@gmail.com>
2018-12-29 09:02:56 +1000

    base before bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'page2' => 'pages#page2'
+  get 'page3' => 'pages#page3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
