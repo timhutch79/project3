d0c07aaab7536fabc018add63b7184b893f34fce
timhutch79git config --global user.name tim.hutch.work@gmail.com <tim.hutch.work@gmail.com>
2018-12-29 16:35:32 +1000

    devise and login and log out done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user do 
+  		get "signin" => 'devise/sessions#new'
+  		delete "signout" => 'devise/sessions#destroy'
+  		get "signup" => 'devise/registrations#new'
+  	end
   root 'pages#home'
   get 'page2' => 'pages#page2'
   get 'page3' => 'pages#page3'
